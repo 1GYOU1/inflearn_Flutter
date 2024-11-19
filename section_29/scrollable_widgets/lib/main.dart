be6079876ac351a81db03283c96999294a67f106
1gyou1 <1gyou1@naver.com>
2024-11-19 23:57:06 +0900

    2024-11-19 section29

---
+++ section_29/scrollable_widgets/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable_widgets/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        useMaterial3: false, // flutter 3.0 이전 버전에서 사용하던 디자인 시스템
+      ),
+      home: homeScreen(),
+    ),
+  );
+}
